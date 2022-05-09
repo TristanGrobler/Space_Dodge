b13ec6362014ea5183818887903fcd1434f3fe17
TristanGrobler <dynamitetechdeals@gmail.com>
2022-05-09 23:16:14 +0200

    code refactor and stream builder of UI.

---
+++ space_dodge/lib/logic.dart
@@ -0,0 +1,177 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:rxdart/rxdart.dart';
+
+class Logic {
+  final double width;
+  final double height;
+
+  Logic({required this.width, required this.height}) {
+    startGame();
+    updateWidthAndBaseFactor(width, height);
+  }
+
+  final _random = Random();
+  late double _widthBase;
+  late double _heightBase;
+  late Timer everySecond;
+  bool collided = false;
+
+  //Declare controllers
+  final _ufo1PositionController = BehaviorSubject<double>();
+  final _ufo2PositionController = BehaviorSubject<double>();
+  final _ufo3PositionController = BehaviorSubject<double>();
+  final _ufo4PositionController = BehaviorSubject<double>();
+  final _ufo5PositionController = BehaviorSubject<double>();
+  final _ufo6PositionController = BehaviorSubject<double>();
+  final _ufo7PositionController = BehaviorSubject<double>();
+  final _ufo8PositionController = BehaviorSubject<double>();
+  final _ufo9PositionController = BehaviorSubject<double>();
+  final _ufo10PositionController = BehaviorSubject<double>();
+  final _rocketPositionController = BehaviorSubject<double>();
+  final _scoreController = BehaviorSubject<int>();
+
+  //Methods to retrieve stream values
+  Stream<double> get ufo1Position => _ufo1PositionController.stream;
+  Stream<double> get ufo2Position => _ufo2PositionController.stream;
+  Stream<double> get ufo3Position => _ufo3PositionController.stream;
+  Stream<double> get ufo4Position => _ufo4PositionController.stream;
+  Stream<double> get ufo5Position => _ufo5PositionController.stream;
+  Stream<double> get ufo6Position => _ufo6PositionController.stream;
+  Stream<double> get ufo7Position => _ufo7PositionController.stream;
+  Stream<double> get ufo8Position => _ufo8PositionController.stream;
+  Stream<double> get ufo9Position => _ufo9PositionController.stream;
+  Stream<double> get ufo10Position => _ufo10PositionController.stream;
+  Stream<double> get rocketPosition => _rocketPositionController.stream;
+  Stream<int> get score => _scoreController.stream;
+
+  ///Start the game.
+  startGame() {
+    _rocketPositionController.sink.add(width / 2);
+    _ufo1PositionController.sink.add(getUFOStartPosition());
+    _ufo2PositionController.sink.add(getUFOStartPosition());
+    _ufo3PositionController.sink.add(getUFOStartPosition());
+    _ufo4PositionController.sink.add(getUFOStartPosition());
+    _ufo5PositionController.sink.add(getUFOStartPosition());
+    _ufo6PositionController.sink.add(getUFOStartPosition());
+    _ufo7PositionController.sink.add(getUFOStartPosition());
+    _ufo8PositionController.sink.add(getUFOStartPosition());
+    _ufo9PositionController.sink.add(getUFOStartPosition());
+    _scoreController.sink.add(0);
+    initialiseTimer();
+  }
+
+  initialiseTimer() {
+    everySecond = Timer.periodic(Duration(milliseconds: 50), (Timer t) {
+      _ufo1PositionController.sink.add(
+        ufoUpdate(_ufo1PositionController.value, 5, 2),
+      );
+      _ufo2PositionController.sink.add(
+        ufoUpdate(_ufo2PositionController.value, 8, 4),
+      );
+      _ufo3PositionController.sink.add(
+        ufoUpdate(_ufo3PositionController.value, 7, 6),
+      );
+      _ufo4PositionController.sink.add(
+        ufoUpdate(_ufo4PositionController.value, 6, 8),
+      );
+      _ufo5PositionController.sink.add(
+        ufoUpdate(_ufo5PositionController.value, 8, 10),
+      );
+      _ufo6PositionController.sink.add(
+        ufoUpdate(_ufo6PositionController.value, 5, 12),
+      );
+      _ufo7PositionController.sink.add(
+        ufoUpdate(_ufo7PositionController.value, 6, 14),
+      );
+      _ufo8PositionController.sink.add(
+        ufoUpdate(_ufo8PositionController.value, 8, 16),
+      );
+      _ufo9PositionController.sink.add(
+        ufoUpdate(_ufo9PositionController.value, 6, 18),
+      );
+
+      //Refresh the UI
+      if (collided == true) {
+        collided = false;
+        print('Score zerod');
+        _scoreController.sink.add(0);
+      }
+    });
+  }
+
+  /// Calculates the new UFO position and if it has collided with the rocket
+  double ufoUpdate(double ufoPosition, double speed, double baseFactor) {
+    var updateRandom = Random();
+    if (ufoPosition - (_heightBase * 18) < (_widthBase + 1) &&
+        ufoPosition - (_heightBase * 18) > -(_widthBase + 1) &&
+        _rocketPositionController.value - (_widthBase * baseFactor) <
+            (_widthBase + 1) &&
+        _rocketPositionController.value - (_widthBase * baseFactor) >
+            -(_widthBase + 1)) {
+      ufoPosition = updateRandom.nextInt(500).toDouble() * -1;
+      collided = true;
+      return ufoPosition;
+    } else if (ufoPosition < _heightBase * 18) {
+      ufoPosition += speed;
+      return ufoPosition;
+    } else {
+      _scoreController.sink.add(_scoreController.value + 1);
+      ufoPosition = updateRandom.nextInt(500).toDouble() * -1;
+      return ufoPosition;
+    }
+  }
+
+  ///Get the start position of the UFO above the top of screen
+  double getUFOStartPosition() {
+    return (_random.nextInt(400).toDouble() * -1) - 100;
+  }
+
+  ///Update the width and base factor which forms the foundation for the "ratio"
+  ///elements build in essence.
+  updateWidthAndBaseFactor(double width, double height) {
+    _widthBase = width / 20;
+    _heightBase = height / 20;
+  }
+
+  ///Move the rocket to the left
+  moveLeft() {
+    if (_rocketPositionController.value > _widthBase * 2) {
+      _rocketPositionController.sink
+          .add(_rocketPositionController.value - _widthBase);
+    }
+  }
+
+  ///Move the rocket to the right
+  moveRight() {
+    if (_rocketPositionController.value < _widthBase * 18) {
+      _rocketPositionController.sink
+          .add(_rocketPositionController.value + _widthBase);
+    }
+  }
+
+  ///Returns the height base factor
+  double hb() {
+    return _heightBase;
+  }
+
+  ///Returns the height base factor
+  double wb() {
+    return _widthBase;
+  }
+
+  /// Dispose game logic
+  dispose() {
+    _ufo1PositionController.close();
+    _ufo2PositionController.close();
+    _ufo3PositionController.close();
+    _ufo4PositionController.close();
+    _ufo5PositionController.close();
+    _ufo6PositionController.close();
+    _ufo7PositionController.close();
+    _ufo8PositionController.close();
+    _ufo8PositionController.close();
+    everySecond.cancel();
+  }
+}
--- space_dodge/lib/main.dart
+++ space_dodge/lib/main.dart
@@ -1,17 +1,21 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'logic.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: LayoutBuilder(builder: (context, BoxConstraints constraints) {
-      return MyApp(
-        height: constraints.maxHeight - 100,
-        width: constraints.maxWidth,
-      );
-    }),
-  ));
+  runApp(
+    MaterialApp(
+      home: LayoutBuilder(
+        builder: (context, BoxConstraints constraints) {
+          return MyApp(
+            height: constraints.maxHeight - 100,
+            width: constraints.maxWidth,
+          );
+        },
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -24,106 +28,59 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var random = Random();
-  late double rocketPosition;
-  late double ufoPosition1;
-  late double ufoPosition2;
-  late double ufoPosition3;
-  late double ufoPosition4;
-  late double ufoPosition5;
-  late double ufoPosition6;
-  late double ufoPosition7;
-  late double ufoPosition8;
-  late double ufoPosition9;
-
-  late double widthBase;
-  late double heightBase;
+  final FocusNode _focusNode = FocusNode();
+  late Logic game = Logic(
+    width: MediaQuery.of(context).size.width,
+    height: MediaQuery.of(context).size.height,
+  );
 
   bool collided = false;
 
-  late Timer everySecond;
-  int score = 0;
+  //int score = 0;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    game.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
-    widthBase = widget.width / 20;
-    heightBase = widget.height / 20;
-    rocketPosition = widget.width / 2;
-    ufoPosition1 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition2 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition3 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition4 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition5 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition6 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition7 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition8 = (random.nextInt(400).toDouble() * -1) - 100;
-    ufoPosition9 = (random.nextInt(400).toDouble() * -1) - 100;
-
-    double ufoUpdate(double ufoPosition, double speed, double baseFactor) {
-      var updateRandom = Random();
-      if (ufoPosition - (heightBase * 18) < (widthBase + 1) &&
-          ufoPosition - (heightBase * 18) > -(widthBase + 1) &&
-          rocketPosition - (widthBase * baseFactor) < (widthBase + 1) &&
-          rocketPosition - (widthBase * baseFactor) > -(widthBase + 1)) {
-        ufoPosition = updateRandom.nextInt(500).toDouble() * -1;
-        collided = true;
-        return ufoPosition;
-      } else if (ufoPosition < heightBase * 18) {
-        ufoPosition += speed;
-        return ufoPosition;
-      } else {
-        score += 1;
-        ufoPosition = updateRandom.nextInt(500).toDouble() * -1;
-        return ufoPosition;
-      }
-    }
-
-    // defines a timer
-    everySecond = Timer.periodic(Duration(milliseconds: 50), (Timer t) {
-      ufoPosition1 = ufoUpdate(ufoPosition1, 5, 2);
-      ufoPosition2 = ufoUpdate(ufoPosition2, 8, 4);
-      ufoPosition3 = ufoUpdate(ufoPosition3, 7, 6);
-      ufoPosition4 = ufoUpdate(ufoPosition4, 6, 8);
-      ufoPosition5 = ufoUpdate(ufoPosition5, 8, 10);
-      ufoPosition6 = ufoUpdate(ufoPosition6, 5, 12);
-      ufoPosition7 = ufoUpdate(ufoPosition7, 6, 14);
-      ufoPosition8 = ufoUpdate(ufoPosition8, 8, 16);
-      ufoPosition9 = ufoUpdate(ufoPosition9, 6, 18);
-
-      //Refresh the UI
-      if (collided == true) {
-        collided = false;
-        print('Score zerod');
-        score = 0;
-      }
-      setState(() {});
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    moveLeft() {
-      if (rocketPosition > widthBase * 2) {
-        rocketPosition -= widthBase;
-      }
-      setState(() {});
+    game.updateWidthAndBaseFactor(
+      MediaQuery.of(context).size.width,
+      MediaQuery.of(context).size.height,
+    );
+
+    void _handleKeyEvent(RawKeyEvent event) {
+      setState(() {
+        String _message =
+            'Not a Q: Pressed 0x${event.logicalKey.keyId.toRadixString(16)}';
+        print(_message);
+
+        if (event.runtimeType.toString() == 'RawKeyDownEvent') {
+          if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+            game.moveLeft();
+          } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+            game.moveRight();
+          }
+        }
+      });
     }
 
-    moveRight() {
-      if (rocketPosition < widthBase * 18) {
-        rocketPosition += widthBase;
-      }
-      setState(() {});
-    }
-
-    Widget ufoWidget(double ufo, double leftFactor) {
-      return Positioned(
-        top: ufo,
-        left: widthBase * leftFactor,
+    Widget ufoWidget(double? ufo, double leftFactor) {
+      return AnimatedPositioned(
+        top: ufo ?? -100,
+        left: game.wb() * leftFactor,
+        duration: Duration(milliseconds: 50),
         child: SizedBox(
-          height: widthBase * 2,
-          width: widthBase * 2,
+          height: game.wb() * 2,
+          width: game.wb() * 2,
           child: Image.asset(
             'images/ufo.png',
             fit: BoxFit.cover,
@@ -132,79 +89,136 @@ class _MyAppState extends State<MyApp> {
       );
     }
 
+    //FocusScope.of(context).requestFocus(_focusNode);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Space Dodge'),
         centerTitle: true,
       ),
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Image.asset(
-              'images/space background.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Positioned(
-            left: 30.0,
-            bottom: 30.0,
-            child: GestureDetector(
-              onTap: moveLeft,
-              child: Container(
-                color: Colors.white30,
-                child: Icon(
-                  Icons.chevron_left,
-                  size: 50.0,
+      body: RawKeyboardListener(
+        focusNode: _focusNode,
+        onKey: _handleKeyEvent,
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(_focusNode);
+            print('grabbed');
+          },
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: Image.asset(
+                  'images/space background.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            right: 30.0,
-            bottom: 30.0,
-            child: GestureDetector(
-              onTap: moveRight,
-              child: Container(
-                color: Colors.white30,
-                child: Icon(
-                  Icons.chevron_right,
-                  size: 50.0,
+              Positioned(
+                left: 30.0,
+                bottom: 30.0,
+                child: GestureDetector(
+                  onTap: game.moveLeft,
+                  child: Container(
+                    color: Colors.white30,
+                    child: Icon(
+                      Icons.chevron_left,
+                      size: 50.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            top: heightBase * 18,
-            left: rocketPosition,
-            child: SizedBox(
-              height: widthBase * 2,
-              width: widthBase * 2,
-              child: Image.asset(
-                'images/rocket.png',
-                fit: BoxFit.cover,
+              Positioned(
+                right: 30.0,
+                bottom: 30.0,
+                child: GestureDetector(
+                  onTap: game.moveRight,
+                  child: Container(
+                    color: Colors.white30,
+                    child: Icon(
+                      Icons.chevron_right,
+                      size: 50.0,
+                    ),
+                  ),
+                ),
               ),
-            ),
+              StreamBuilder<double>(
+                  stream: game.rocketPosition,
+                  builder: (context, snapshot) {
+                    return Positioned(
+                      top: game.hb() * 18,
+                      left: snapshot.data == null ? -100 : snapshot.data,
+                      child: SizedBox(
+                        height: game.wb() * 2,
+                        width: game.wb() * 2,
+                        child: Image.asset(
+                          'images/rocket.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo1Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 2);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo2Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 4);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo3Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 6);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo4Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 8);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo5Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 10);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo6Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 12);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo7Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 14);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo8Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 16);
+                  }),
+              StreamBuilder<double>(
+                  stream: game.ufo9Position,
+                  builder: (context, snapshot) {
+                    return ufoWidget(snapshot.data, 18);
+                  }),
+              StreamBuilder<int>(
+                  stream: game.score,
+                  builder: (context, snapshot) {
+                    return Positioned(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Score: ${snapshot.data ?? 0}',
+                          style: TextStyle(color: Colors.white60),
+                        ),
+                      ),
+                    );
+                  })
+            ],
           ),
-          ufoWidget(ufoPosition1, 2),
-          ufoWidget(ufoPosition2, 4),
-          ufoWidget(ufoPosition3, 6),
-          ufoWidget(ufoPosition4, 8),
-          ufoWidget(ufoPosition5, 10),
-          ufoWidget(ufoPosition6, 12),
-          ufoWidget(ufoPosition8, 14),
-          ufoWidget(ufoPosition9, 16),
-          Positioned(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                'Score: $score',
-                style: TextStyle(color: Colors.white60),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
